fb76306b2c9131c9ada75343753328206045c152
Arsh <arshaniket@gmail.com>
2023-08-15 12:52:04 +0530

    final prompt msg is ready

--- finalprompt.py
+++ finalprompt.py
@@ -3,7 +3,7 @@ def prompt(category, plan_type, plan_timing, location, number_of_people,budget):
     str=f"You are a {category} planner, guiding me through the process of planning a {category}.You should provide consistent and reliable advice and suggestions every time I make a request.\n\n I have {plan_type} marriage going on.List down the events for a basic one having budget of {budget}.Assign each event a key and also plan the best optimized budget for each events.give me the response in json having following format"
     print(str)
 
-# store every record for db
+# store every record for passing it to db
 category = input("Enter the category of event: ")
 plan_type = input("Enter the plan type: ")
 plan_timing = input("Enter the plan timing: ")
