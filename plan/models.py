a6ec30d04b0119e883b505773a80d6d3703e48a2
AshishJukaria <ashishjukaria2001@gmail.com>
2023-08-17 17:27:18 +0530

    models

--- plan/models.py
+++ plan/models.py
@@ -1,3 +1,69 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+from django.conf import settings
+
+
+class Plans(models.Model):
+    # category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
+
+
+class EventTypes(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Events(models.Model):
+    type = models.ForeignKey(EventTypes, on_delete=models.CASCADE)
+    plan_id = models.ForeignKey(Plans, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    image = models.ImageField(upload_to="event_images/")
+
+    def __str__(self):
+        return self.title
+
+
+class Subscriptions(models.Model):
+    title = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="subscriptions_plan",
+    )
+    desc = models.TextField()
+    supported_features = models.TextField()
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title.username
+
+
+class ItineraryTypes(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Itinerary(models.Model):
+    event_id = models.ForeignKey("Events", on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    is_active = models.BooleanField(default=True)
+    type = models.ForeignKey(ItineraryTypes, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
--- plan/urls.py
+++ plan/urls.py
@@ -1,9 +1,40 @@
 # urls.py
 from django.urls import path
-from .views import gpt_response_view
+from .views import *
 
 urlpatterns = [
- path('gpt-response/', gpt_response_view, name='gpt_response'),
- 
- ]
-
+    path("gpt-response/", gpt_response_view, name="gpt_response"),
+    path("gpt-response/", gpt_response_view, name="gpt_response"),
+    path(
+        "subscriptions/",
+        SubscriptionListCreateView.as_view(),
+        name="subscription-list-create",
+    ),
+    path(
+        "subscriptions/<int:pk>/",
+        SubscriptionRetrieveUpdateDeleteView.as_view(),
+        name="subscription-retrieve-update-delete",
+    ),
+    path("plans/", PlansListCreateView.as_view(), name="plans-list-create"),
+    path(
+        "plans/<int:pk>/",
+        PlansRetrieveUpdateDeleteView.as_view(),
+        name="plans-retrieve-update-delete",
+    ),
+    path("events/", EventsListCreateView.as_view(), name="events-list-create"),
+    path(
+        "events/<int:pk>/",
+        EventsRetrieveUpdateDeleteView.as_view(),
+        name="events-retrieve-update-delete",
+    ),
+    path(
+        "itineraries/",
+        ItineraryListCreateView.as_view(),
+        name="itinerary-list-create",
+    ),
+    path(
+        "itineraries/<int:pk>/",
+        ItineraryRetrieveUpdateDeleteView.as_view(),
+        name="itinerary-retrieve-update-delete",
+    ),
+]
