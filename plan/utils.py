ac0656ae2fb2435d63a4ebce979843cf40821bd8
AshishJukaria <ashishjukaria2001@gmail.com>
2023-08-26 16:18:25 +0530

    Render and PARser

--- plan/utils.py
+++ plan/utils.py
@@ -1,4 +1,7 @@
 import requests
+from rest_framework.renderers import JSONRenderer
+import re
+from rest_framework.parsers import JSONParser
 
 def generate_gpt_response(prompt_text):
     openai_api_key="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"
@@ -16,7 +19,43 @@ def generate_gpt_response(prompt_text):
     else:
         return None
 
+class CamelCaseJSONRenderer(JSONRenderer):
+    def render(self, data, accepted_media_type=None, renderer_context=None):
+        new_data = self.convert_keys_to_camel_case(data)
+        return super().render(new_data, accepted_media_type, renderer_context)
 
+    def convert_keys_to_camel_case(self, data):
+        import ipdb
+        ipdb.set_trace
+        if isinstance(data, dict):
+            new_data = {}
+            for key, value in data.items():
+                new_key = re.sub(r"_([a-z])", lambda m: m.group(1).upper(), key)
+                new_data[new_key] = self.convert_keys_to_camel_case(value)
+            return new_data
+        elif isinstance(data, list):
+            return [self.convert_keys_to_camel_case(item) for item in data]
+        else:
+            return data
+
+
+class CamelCaseJSONParser(JSONParser):
+    def parse(self, stream, media_type=None, parser_context=None):
+        data = super().parse(stream, media_type, parser_context)
+        return self.convert_keys_to_snake_case(data)
+
+    def convert_keys_to_snake_case(self, data):
+
+        if isinstance(data, dict):
+            new_data = {}
+            for key, value in data.items():
+                new_key = re.sub(r"(?<!^)(?=[A-Z])", "_", key).lower()
+                new_data[new_key] = self.convert_keys_to_snake_case(value)
+            return new_data
+        elif isinstance(data, list):
+            return [self.convert_keys_to_snake_case(item) for item in data]
+        else:
+            return data
 
 
 
