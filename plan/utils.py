05438c1f486defc7c85afeeb278f473ebbba8eb3
AshishJukaria <ashishjukaria2001@gmail.com>
2023-08-28 20:49:54 +0530

    Change open ai

--- plan/utils.py
+++ plan/utils.py
@@ -1,20 +1,27 @@
 import requests
-import re
-
+import os
 
 def generate_gpt_response(prompt_text):
-    openai_api_key="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"
+
+    openai_api_key = os.environ.get("OPENAI_API_KEY")  # Access the environment variable
 
     response = requests.post(
-        "https://api.openai.com/v1/engines/gpt-3.5-turbo/completions",
+        "https://api.openai.com/v1/chat/completions",  # Use the chat completions endpoint
         headers={"Authorization": f"Bearer {openai_api_key}"},
-        json={"prompt": prompt_text, "max_tokens": 50},  # Adjust parameters as needed
+        json={
+            "messages": [
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": prompt_text}
+            ],
+            "model": "gpt-3.5-turbo"  # Specify the model parameter
+        },
     )
     data = response.json()
     
     if "choices" in data:
-        completion_text = data["choices"][0]["text"]
+        completion_text = data["choices"][0]["message"]["content"]
         return completion_text
     else:
         return None
+
 
