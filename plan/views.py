7efc5ea577c4f7e8a8e8328e87bbff4dfd1fd926
Arsh <arshaniket@gmail.com>
2023-09-12 14:27:46 +0530

    service prompt finalised

--- plan/views.py
+++ plan/views.py
@@ -27,7 +27,22 @@ def gpt_response_view(request):
     else:
         return JsonResponse({"error": "Invalid request method"})
 
+from plan.models import Prompts, Category
+from .scripts.serviceprompt import ServicePrompt
+import sqlite3
+def get_prompt_view(request):
+    if request.method == 'GET':
+        key = request.GET.get('key')
+        category = request.GET.get('category')
+        plan_type = request.GET.get('plan_type')
+        budget = request.GET.get('budget')
+        db_connection=sqlite3.connect(".db")
+        service = ServicePrompt(db_connection)
+        formatted_prompt = service.get_prompt(key, category, plan_type, budget)
+        db_connection.close()
+        return JsonResponse({'prompt': formatted_prompt})
 
+    return JsonResponse({'error': 'Invalid request method'}, status=400)
 
 class SubscriptionListCreateView(generics.ListCreateAPIView):
     queryset = Subscriptions.objects.all()
@@ -67,7 +82,7 @@ class ItineraryListCreateView(generics.ListCreateAPIView):
 class ItineraryRetrieveUpdateDeleteView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Itinerary.objects.all()
     serializer_class = ItinerarySerializer
-=======
+
 class CategoryList(generics.ListCreateAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
